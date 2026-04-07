10831b37cde78c789693a6dc62d950cd05b7d070
Zailey <zmw24@nau.edu>
2026-04-07 15:11:34 -0700

    Add files via upload

---
+++ test_package/TestPackage/R/MaxTemp.R
@@ -0,0 +1,9 @@
+#' A time series of daily maximum temperatures in Flagstaff, AZ.
+#'
+#' @format a data frame with 10882 observations
+#' \describe{
+#'   \item{Date}{The date of observation as a POSIX date format.}
+#'   \item{MaxTemp}{Daily maximum temperature in degrees Farhenheit.}
+#' }
+#' @source \url{www.ncdc.noaa.gov}
+"MaxTemp"
